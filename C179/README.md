49827ff63a36b3fbe724fba3aa8c50f0fdc1b48b
SHAHAZADI SHAGUFTHA SYED <64363861+SHAHAZADISHAGUFTHA@users.noreply.github.com>
2022-02-12 23:40:03 +0530

    Add files via upload

---
+++ C179/README.md
@@ -0,0 +1,1 @@
+# PRO-C179-Project-Sloution
---
+++ C179/ar_weather.js
@@ -0,0 +1,20 @@
+let coordinates = {}
+
+$(document).ready(function () {
+    get_coordinates();
+})
+
+function get_coordinates() {
+    let searchParams = new URLSearchParams(window.location.search)
+    if (searchParams.has('source') && searchParams.has('destination')) {
+        let source = searchParams.get('source')
+        let destination = searchParams.get('destination')
+        coordinates.source_lat = source.split(";")[0]
+        coordinates.source_lon = source.split(";")[1]
+        coordinates.destination_lat = destination.split(";")[0]
+        coordinates.destination_lon = destination.split(";")[1]
+    } else {
+        alert("Coordinates not selected!")
+        window.history.back();
+    }
+}
